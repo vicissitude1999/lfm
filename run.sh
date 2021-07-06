b98a64166c09932b29702ebd3040f7e957c2e55f
vicissitude1999 <tangrenyi99@gmail.com>
2021-07-07 00:19:12 +0800

    batch size change

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 nvidia-smi
 #conda env create -f environment.yaml
 source activate mytorch
-python train_search_ts.py --weight_lambda 1 --weight_gamma 1 --unrolled --is_cifar100 0 --gpu 0 --save debugging --batch_size 32
+python train_search_ts.py --weight_lambda 1 --weight_gamma 1 --unrolled --is_cifar100 0 --gpu 0 --save debugging --batch_size 18
