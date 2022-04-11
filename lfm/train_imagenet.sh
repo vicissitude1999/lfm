7c4f7935c054aa5e4075bc865c3afe9a4c8c9d19
renyi <renyi.tang@yahoo.com>
2022-04-11 08:44:36 +0800

    train imagenet

--- lfm/train_imagenet.sh
+++ lfm/train_imagenet.sh
@@ -4,4 +4,5 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 lfm/train_imagenet.py \
 --seed 0 \
 --save outputs/darts-lfm/cifar10_on_imagenet/DEBUG \
+--batch_size 96 \
 --arch DARTS_CIFAR10_LFM_F3
